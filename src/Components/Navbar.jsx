714d45523ff0b281d9d2b9a659b4d7e1d1f402d9
Luke Hancock <lukehancock42@gmail.com>
2024-09-18 16:17:44 +0100

    removed debugging function

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,9 +3,6 @@ import { Link } from 'react-scroll'
 
 export default function Navbar() {
 
-  const handleSetActive = (to) => {
-    console.log(`Active section: ${to}`);
-  };
   
   return (
     <nav className="bg-white font-bold sticky top-0 z-10">
@@ -14,7 +11,7 @@ export default function Navbar() {
             <Link to="Hero" smooth={true} duration={500} offset={-56} className="cursor-pointer ">Luke Hancock</Link>
         </div>
         <div className='flex items-center space-x-4 md:space-x-12'>
-            <Link to="Hero" smooth={true} duration={500}  offset={-56} className="cursor-pointer hover:underline" activeClass="text-blue-500"  onSetActive={handleSetActive}  spy={true}>Home</Link>
+            <Link to="Hero" smooth={true} duration={500}  offset={-56} className="cursor-pointer hover:underline" activeClass="text-blue-500" spy={true}>Home</Link>
             <Link to="Projects" smooth={true} duration={500} offset={-56} className="cursor-pointer hover:underline " activeClass="text-blue-500" spy={true}>Projects</Link>
             <Link to="Contact" smooth={true} duration={500} offset={-56} className="cursor-pointer hover:underline " activeClass="text-blue-500"  spy={true}>Contact</Link>
         </div>

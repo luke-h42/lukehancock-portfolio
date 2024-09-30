01977593c83a62511f0676a2da7140dc1b731fa6
Luke Hancock <lukehancock42@gmail.com>
2024-09-30 15:04:13 +0100

    added new projects

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,21 +1,56 @@
-import { useState } from 'react'
-import { Link } from 'react-scroll'
+import { Link } from "react-scroll";
 
 export default function Navbar() {
-
-  
   return (
     <nav className="bg-white font-bold sticky top-0 z-10">
-      <div className='py-4 flex items-center justify-between container mx-auto px-6 max-w-screen-xl '>
-        <div className=''>
-            <Link to="Hero" smooth={true} duration={500} offset={-56} className="cursor-pointer ">Luke Hancock</Link>
+      <div className="py-4 flex items-center justify-between container mx-auto px-6 max-w-screen-xl ">
+        <div className="">
+          <Link
+            to="Hero"
+            smooth={true}
+            duration={500}
+            offset={-56}
+            className="cursor-pointer "
+          >
+            Luke Hancock
+          </Link>
         </div>
-        <div className='flex items-center space-x-4 md:space-x-12'>
-            <Link to="Hero" smooth={true} duration={500}  offset={-56} className="cursor-pointer hover:underline" activeClass="text-blue-500" spy={true}>Home</Link>
-            <Link to="Projects" smooth={true} duration={500} offset={-56} className="cursor-pointer hover:underline " activeClass="text-blue-500" spy={true}>Projects</Link>
-            <Link to="Contact" smooth={true} duration={500} offset={-56} className="cursor-pointer hover:underline " activeClass="text-blue-500"  spy={true}>Contact</Link>
+        <div className="flex items-center space-x-4 md:space-x-12">
+          <Link
+            to="Hero"
+            smooth={true}
+            duration={500}
+            offset={-56}
+            className="cursor-pointer hover:underline"
+            activeClass="text-blue-500"
+            spy={true}
+          >
+            Home
+          </Link>
+          <Link
+            to="Projects"
+            smooth={true}
+            duration={500}
+            offset={-56}
+            className="cursor-pointer hover:underline "
+            activeClass="text-blue-500"
+            spy={true}
+          >
+            Projects
+          </Link>
+          <Link
+            to="Contact"
+            smooth={true}
+            duration={500}
+            offset={-56}
+            className="cursor-pointer hover:underline "
+            activeClass="text-blue-500"
+            spy={true}
+          >
+            Contact
+          </Link>
         </div>
-        </div>
+      </div>
     </nav>
-  )
+  );
 }
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,30 +1,100 @@
-import React from 'react'
-import weather from '../assets/weather_dashboard_site_preview.jpg'
-import login from '../assets/login_mern_site_preview.jpg'
-import food from '../assets/recipe_site_preview_2.jpg'
-import Project_item from './project-item/project_item'
+import React from "react";
+import weather from "../assets/weather_dashboard_site_preview.jpg";
+import login from "../assets/login_mern_site_preview.jpg";
+import food from "../assets/recipe_site_preview_2.jpg";
+import lamyaAkhiyat from "../assets/akhiyatlamya.com_.png";
+import lukeHancock from "../assets/lukehancock.dev_.png";
+import swimsuccess from "../assets/swimsuccess.png";
+import Project_item from "./project-item/project_item";
 
 const projects = [
-    {title: "Login Page", src: login , alt: "Login page preview image", description: "The login page project was created to test my skills as a backend developer.", technology: "React, Express, Tailwind, MongoDB", live: "https://login-mern-seven.vercel.app/", code: "https://github.com/luke-h42/login-mern"   },
-    {title: "Homepage", src: weather, alt: "Weather dashboard preview image", description: "The homepage app tested my skills to build a server that can handle multitple APIs.", technology:"React with typescript, Express", live: "https://weather-dashboard-beta-self.vercel.app/", code: "https://github.com/luke-h42/weather-dashboard"},
-    {title: "Food Recipes", src: food, alt:"Food recipes preview image", description: "The food recipe app was my first dive into building a server to handle an api request as well as saving data to a database.", technology: "React, Express, Supabase", live:"https://food-recipe-app-jet-chi.vercel.app/", code:"https://github.com/luke-h42/food-recipe-app"},
-
-]
+  {
+    title: "SwimSuccess",
+    src: swimsuccess,
+    alt: "Swim Success preview image",
+    description:
+      "A frontend page as a personal project. This project was to test my frontend skills including web design to make a website that was business ready.",
+    technology: "React, Tailwind",
+    live: "https://swimsuccess.netlify.app/",
+    code: "https://github.com/luke-h42/swimsuccess",
+  },
+  {
+    title: "Lamya Akhiyat",
+    src: lamyaAkhiyat,
+    alt: "Lamya akhiyat preview image",
+    description:
+      "A frontend page made for the client's business as a speech therapist. This project included domain registering and editing DNS records. The code is private for client privacy.  ",
+    technology: "React, Vanilla CSS",
+    live: "https://akhiyatlamya.com/",
+    code: "https://akhiyatlamya.com/",
+  },
+  {
+    title: "Luke Hancock Personal Porfolio",
+    src: lukeHancock,
+    alt: "Luke Hancock website preview image",
+    description:
+      "My personal portfolio website made to showcase all the projects and learning that I've been doing.   ",
+    technology: "React, Tailwind",
+    live: "https://lukehancock.dev/",
+    code: "https://github.com/luke-h42/lukehancock-portfolio",
+  },
+  {
+    title: "Login Page",
+    src: login,
+    alt: "Login page preview image",
+    description:
+      "The login page project was created to test my skills as a backend developer.",
+    technology: "React, Express, Tailwind, MongoDB",
+    live: "https://login-mern-seven.vercel.app/",
+    code: "https://github.com/luke-h42/login-mern",
+  },
+  {
+    title: "Homepage",
+    src: weather,
+    alt: "Weather dashboard preview image",
+    description:
+      "The homepage app tested my skills to build a server that can handle multitple APIs.",
+    technology: "React with typescript, Express",
+    live: "https://weather-dashboard-beta-self.vercel.app/",
+    code: "https://github.com/luke-h42/weather-dashboard",
+  },
+  {
+    title: "Food Recipes",
+    src: food,
+    alt: "Food recipes preview image",
+    description:
+      "The food recipe app was my first dive into building a server to handle an api request as well as saving data to a database.",
+    technology: "React, Express, Supabase",
+    live: "https://food-recipe-app-jet-chi.vercel.app/",
+    code: "https://github.com/luke-h42/food-recipe-app",
+  },
+];
 
 export default function Projects() {
   return (
-    <div className='bg-gray-100 shadow-inner min-h-screen flex items-center justify-center pt-5 pb-5' id="Projects">
-    <div className='container mx-auto max-w-screen-xl px-6 '>
-    <div className='text-4xl mb-10 font-body'>Projects</div>
-    <div className="flex justify-center">
-    <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 ">
-      {projects.map((project, index) => 
-        <Project_item title={project.title} image={project.src} alt={project.alt} description={project.description} technology={project.technology} key={index} live={project.live} code={project.code} />
-      )}
+    <div
+      className="bg-gray-100 shadow-inner min-h-screen flex items-center justify-center pt-5 pb-5"
+      id="Projects"
+    >
+      <div className="container mx-auto max-w-screen-xl px-6 ">
+        <div className="text-4xl mb-10 font-body">Projects</div>
+        <div className="flex justify-center">
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 ">
+            {projects.map((project, index) => (
+              <Project_item
+                title={project.title}
+                image={project.src}
+                alt={project.alt}
+                description={project.description}
+                technology={project.technology}
+                key={index}
+                live={project.live}
+                code={project.code}
+              />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-  </div>
-  )
+  );
 }
-
